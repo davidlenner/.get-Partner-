3c13b6cdb70e90ff9e799dd79528e8f9e055133c
danielszakacs <daniel.sokach@gmail.com>
2018-10-02 11:28:54 +0200

    DB in_it_ file is done

---
+++ src/main/java/com/codecool/getpartner/sql/in_it_.sql
@@ -0,0 +1,19 @@
+CREATE TABLE  users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  email VARCHAR (30),
+  password VARCHAR (50)
+);
+
+CREATE TABLE profile(
+  userId INT NOT NULL, /* Ez itt egy fk */
+  username VARCHAR (20),
+  age INT,
+  room VARCHAR (20),
+  picture TEXT,
+  favoriteLanguage VARCHAR (20),
+  fromTO TEXT,
+  bio TEXT
+);
+
+ALTER TABLE ONLY profile
+  ADD CONSTRAINT fk_user_id FOREIGN KEY (userId) REFERENCES users(id);
